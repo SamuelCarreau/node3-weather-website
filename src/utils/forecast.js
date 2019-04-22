7757f9ae85ecd0b8156df9bbb9ad5d5803bf883c
samuel carreau <samuelcarreau@gmail.com>
2019-04-22 10:45:09 -0400

    new string syntax

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,25 +1,15 @@
 const request = require('request')
-const util = require('util')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = util.format(
-        //  ex : https://api.darksky.net/forecast/7902226b1004bb9045d9414a1fe5f724/46,-71?units=si
-        'https://api.darksky.net/forecast/7902226b1004bb9045d9414a1fe5f724/%d,%d?units=si',
-        latitude,
-        longitude,
-    )
+    //  ex : https://api.darksky.net/forecast/7902226b1004bb9045d9414a1fe5f724/46,-71?units=si
+    const url = `https://api.darksky.net/forecast/7902226b1004bb9045d9414a1fe5f724/${latitude},${longitude}?units=si`
     request({url, json: true}, (error, {body}) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback(body.error, undefined)
         } else {
-            callback(undefined, util.format('%s It is currently %s degrees out. There is a %s% chance of rain. The highest temperature for the day is %s dregrees and the lowest is %s degrees',
-                body.daily.data[0].summary,
-                body.currently.temperature,
-                body.currently.precipProbability*100,
-                body.daily.data[0].temperatureMax,
-                body.daily.data[0].temperatureMin))
+            callback(undefined,`${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability*100}% chance of rain. The highest temperature for the day is ${body.daily.data[0].temperatureMax} dregrees and the lowest is ${body.daily.data[0].temperatureMin} degrees`)
         }
     })
 }

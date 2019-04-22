b934d5d99535c06eda556e3ff32817c6febda699
samuel carreau <samuelcarreau@gmail.com>
2019-04-22 01:09:43 -0400

    add min and max temp of the day in the description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,6 +3,7 @@ const util = require('util')
 
 const forecast = (latitude, longitude, callback) => {
     const url = util.format(
+        //  ex : https://api.darksky.net/forecast/7902226b1004bb9045d9414a1fe5f724/46,-71?units=si
         'https://api.darksky.net/forecast/7902226b1004bb9045d9414a1fe5f724/%d,%d?units=si',
         latitude,
         longitude,
@@ -13,10 +14,12 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback(body.error, undefined)
         } else {
-            callback(undefined, util.format('%s It is currently %s degrees out. There is a %s% chance of rain.',
+            callback(undefined, util.format('%s It is currently %s degrees out. There is a %s% chance of rain. The highest temperature for the day is %s dregrees and the lowest is %s degrees',
                 body.daily.data[0].summary,
                 body.currently.temperature,
-                body.currently.precipProbability*100))
+                body.currently.precipProbability*100,
+                body.daily.data[0].temperatureMax,
+                body.daily.data[0].temperatureMin))
         }
     })
 }
